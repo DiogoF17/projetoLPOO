48c870c0c1f75983372bda3645577e195032f771
diogo santos <diogofilipe1_santos@hotmail.com>
2020-04-11 14:25:38 +0100

    Comecei a fazer os testes

--- src/test/java/PauseMenuTest.java
+++ src/test/java/PauseMenuTest.java
@@ -1,2 +1,80 @@
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
 public class PauseMenuTest {
+
+    @Test
+    public void arrowDownTest(){
+        PauseMenu pauseMenu = new PauseMenu(5, 6);
+        Game gameMock = Mockito.mock(Game.class);
+
+        assertEquals(1, pauseMenu.getHighlighted());
+        assertEquals(-2, pauseMenu.getPosition().getX());
+        assertEquals(0, pauseMenu.getPosition().getY());
+
+        pauseMenu.arrowDown(gameMock);
+        assertEquals(2, pauseMenu.getHighlighted());
+
+        pauseMenu.arrowDown(gameMock);
+        assertEquals(3, pauseMenu.getHighlighted());
+
+        pauseMenu.arrowDown(gameMock);
+        assertEquals(4, pauseMenu.getHighlighted());
+
+        pauseMenu.arrowDown(gameMock);
+        assertEquals(1, pauseMenu.getHighlighted());
+    }
+
+    @Test
+    public void arrowUpTest(){
+        PauseMenu pauseMenu = new PauseMenu(5, 6);
+        Game gameMock = Mockito.mock(Game.class);
+
+        assertEquals(1, pauseMenu.getHighlighted());
+        assertEquals(-2, pauseMenu.getPosition().getX());
+        assertEquals(0, pauseMenu.getPosition().getY());
+
+        pauseMenu.arrowUp(gameMock);
+        assertEquals(4, pauseMenu.getHighlighted());
+
+        pauseMenu.arrowUp(gameMock);
+        assertEquals(3, pauseMenu.getHighlighted());
+
+        pauseMenu.arrowUp(gameMock);
+        assertEquals(2, pauseMenu.getHighlighted());
+
+        pauseMenu.arrowUp(gameMock);
+        assertEquals(1, pauseMenu.getHighlighted());
+    }
+
+    @Test
+    public void arrowLeftTest(){
+        PauseMenu pauseMenu = new PauseMenu(5, 6);
+        Game gameMock = Mockito.mock(Game.class);
+
+        assertEquals(1, pauseMenu.getHighlighted());
+        assertEquals(-2, pauseMenu.getPosition().getX());
+        assertEquals(0, pauseMenu.getPosition().getY());
+
+        pauseMenu.arrowLeft(gameMock);
+        assertEquals(1, pauseMenu.getHighlighted());
+
+    }
+
+    @Test
+    public void arrowRightTest(){
+        PauseMenu pauseMenu = new PauseMenu(5, 6);
+        Game gameMock = Mockito.mock(Game.class);
+
+        assertEquals(1, pauseMenu.getHighlighted());
+        assertEquals(-2, pauseMenu.getPosition().getX());
+        assertEquals(0, pauseMenu.getPosition().getY());
+
+        pauseMenu.arrowRight(gameMock);
+        assertEquals(1, pauseMenu.getHighlighted());
+
+    }
 }
+
