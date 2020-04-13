388b49e98a85c65a05cb411689759412a1c1ad27
diogo santos <diogofilipe1_santos@hotmail.com>
2020-04-13 15:39:01 +0100

    Fiz mais uns testes

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,4 @@
+import com.sun.tools.javac.Main;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -6,24 +7,72 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 
 public class GameTest {
 
-    Game game;
-
+    Arena arenaMock;
     List<GameObserver> gameObservers;
 
     @Before
     public void setUp(){
-        Arena arena = Mockito.mock(Arena.class);
-        game = new Game(arena);
+        arenaMock = Mockito.mock(Arena.class);
+    }
 
-        gameObservers = new ArrayList<>();
+    @Test
+    public void getFinishTest() {
+        Game game = new Game(arenaMock);
 
-        gameObservers.add(new Application());
-        gameObservers.add(new Application());
+        assertFalse(game.getFinish());
+    }
+
+    @Test
+    public void setFinishTest() {
+        Game game = new Game(arenaMock);
+
+        game.setFinish(true);
+        assertTrue(game.getFinish());
+    }
+
+    @Test
+    public void getArenaTest() {
+        Game game = new Game(arenaMock);
+
+        Arena arena = game.getArena();
+
+        assertEquals(arenaMock, arena);
+    }
+
+    @Test
+    public void setArenaTest() {
+        Game game = new Game(arenaMock);
+        Arena arenaMock2 = Mockito.mock(Arena.class);
+
+        game.setArena(arenaMock2);
+        Arena arena = game.getArena();
+
+        assertEquals(arenaMock2, arena);
+    }
+
+    @Test
+    public void changeStateTest(){
+        Game game = new Game(arenaMock);
+        MainMenu stateMock = Mockito.mock(MainMenu.class);
+
+        game.changeState(stateMock);
+        State state = game.getState();
+
+        assertEquals(stateMock, state);
+    }
+
+    @Test
+    public void getStateTest(){
+        Game game = new Game(arenaMock);
+
+        State state = game.getState();
+
+        assertEquals(MainMenu.class, state.getClass());
     }
 
     @Test
